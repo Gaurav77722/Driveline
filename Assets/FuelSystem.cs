74d09231b12b232c726256f3472349887f7d8a8b
Ari Garcia <71861098+AriGarcia-tech@users.noreply.github.com>
2022-10-05 13:22:01 -0700

    incompletefuelscript
    Fuel counter drops, but need to be refilled from fuel pumps

--- Assets/FuelSystem.cs
+++ Assets/FuelSystem.cs
@@ -2,29 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class FuelSystem : MonoBehaviour
 {
-    public Text Fueltext;
-    public Image FuelImage;
+    public float currentFuel = 100f;
+    public TextMeshProUGUI fuelText;
+    public float gameTick = 1f;
+    float timeAccumulated;
+    //bool carIsMoving;
 
-    public float CurrentTime = 5;
-    public float MaxTime = 5;
-
-    void FixedUpdate()
+    public void Update()
     {
-        UpdateUI();
-        
-
-        
+        UpdateFuel();
     }
-    public void UpdateUI()
-        {
-        //FuelImage.fillAmount = CurrentTime/MaxTime ;
-        //Fueltext.text = "Fuel: " + (CurrentTime/MaxTime * 100).ToString();
+    private void UpdateFuel(){
+        timeAccumulated += Time.deltaTime;
+
+        if(timeAccumulated > gameTick){
+            if(currentFuel> 0){
+                currentFuel -= 1f;
+                timeAccumulated = 0f;
+            }
+            else{
+                currentFuel -= 0;
+            }
+            fuelText.text = currentFuel.ToString();
         }
-    private void LateUpdate()
+    }
+    private void OnTriggerEnter(Collider collider)
     {
-        
+        if(collider.tag == "fuel")
+        {
+            currentFuel = 100f;
+        }
     }
 }
