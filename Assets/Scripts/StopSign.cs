22e14753c23b9abfac5f09717ec8a17adc40a7dd
csumbpradnya <74765231+csumbpradnya@users.noreply.github.com>
2022-10-05 23:09:24 -0700

    updated map with offroad

--- Assets/Scripts/StopSign.cs
+++ Assets/Scripts/StopSign.cs
@@ -1,19 +1,24 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class StopSign : MonoBehaviour
 {
- private bool carStopped = false;
+
+ private bool carStopped;
+
+
  private void OnTriggerStay(Collider other)
  {
   if (other.tag == "Car")
   {
-   if (other.attachedRigidbody.velocity.magnitude*3.6f <= 1f)
-   {
-    carStopped = true;
-   }
+   
+    if (other.attachedRigidbody.velocity.magnitude * 3.6f <= 1f)
+    {
+     carStopped = true;
+    }
   }
  }
 
@@ -25,7 +30,13 @@ public class StopSign : MonoBehaviour
   }
   else
   {
-   Debug.Log("Car Stopped!");
+   Debug.Log("Car Stopped");
   }
+
+  carStopped = false;
  }
+
 }
+
+ 
+
